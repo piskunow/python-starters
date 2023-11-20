d7bba6590d857cdf9ab25c4ba7028a6bc06dedfa
Pablo Piskunow <pablo.perez.piskunow@gmail.com>
2023-11-20 11:05:14 +0100

    fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,26 @@
 """Test cases for the __main__ module."""
 import pytest
 from typer.testing import CliRunner
+
 from python_starters.__main__ import app
+
 
 @pytest.fixture
 def runner() -> CliRunner:
     """Fixture for invoking command-line interfaces."""
     return CliRunner()
 
+
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(app, ["--help"])  # Using "--help" to trigger the CLI without executing a command
+    result = runner.invoke(
+        app, ["--help"]
+    )  # Using "--help" to trigger the CLI without executing a command
     assert result.exit_code == 0
+
 
 def test_init_command(runner: CliRunner) -> None:
     """Test the init command."""
     result = runner.invoke(app, ["init"])
     assert result.exit_code == 0
     # Add more assertions here based on the expected output
-
-def test_add_command(runner: CliRunner) -> None:
-    """Test the add command."""
-    result = runner.invoke(app, ["add", "https://github.com/your-username/your-starter-repo.git"])
-    assert result.exit_code == 0
-    # Add more assertions here based on the expected output
-
-# Continue writing tests for the other commands (update, remove, list, resolve) in a similar manner
