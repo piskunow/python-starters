5b9ae0e30713457a90cbd6e087211c07d2a6401d
Pablo Piskunow <pablo.perez.piskunow@gmail.com>
2023-11-21 10:07:18 +0100

    fix import

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
 """Test cases for the __main__ module."""
 import pytest
 from click.testing import CliRunner
-from starters_cli import cli
+
+from python_starters.__main__ import main
 
 
 @pytest.fixture
@@ -13,13 +14,13 @@ def runner() -> CliRunner:
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(
-        cli, ["--help"]
+        main, ["--help"]
     )  # Using "--help" to trigger the CLI without executing a command
     assert result.exit_code == 0
 
 
 def test_init_command(runner: CliRunner) -> None:
     """Test the init command."""
-    result = runner.invoke(cli, ["init"])
+    result = runner.invoke(main, ["init"])
     assert result.exit_code == 0
     # Add more assertions here based on the expected output
