20afa2eb0420d966715910c819f2c9259f8cace4
Pablo Piskunow <pablo.perez.piskunow@gmail.com>
2023-11-21 12:52:10 +0100

    test that cookiecutter is called

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -2,7 +2,8 @@
 from unittest.mock import mock_open
 from unittest.mock import patch
 
-from python_starters.convert_templates import save_starter_config
+from python_starters.starters_api import initialize_starter
+from python_starters.starters_api import save_starter_config
 
 
 def test_save_starter_config():
@@ -21,3 +22,13 @@ def test_save_starter_config():
 
         # Check if .startersrc was written
         mock_file.assert_called_with("/path/to/project/.startersrc", "w")
+
+
+def test_initialize_starter_calls_cookiecutter():
+    """Test that cookiecutter is called."""
+    template_url = "https://example.com/cookiecutter-repo.git"
+
+    with patch("python_starters.starters_api.cookiecutter") as mock_cookiecutter:
+        initialize_starter(template_url)
+
+        mock_cookiecutter.assert_called_once_with(template_url, no_input=False)
